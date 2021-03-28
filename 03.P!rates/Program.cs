5cfe687cbc496b19ceb990a6ae33ce7d87cb7979
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2021-03-28 12:04:22 +0300

    Update Program.cs
    code optimization

--- 03.P!rates/Program.cs
+++ 03.P!rates/Program.cs
@@ -15,9 +15,84 @@ namespace _03.P_rates
     {
         static void Main(string[] args)
         {
-            Dictionary<string, City> citys = new Dictionary<string, City>();
+            Dictionary<string, City> citys = ReadCity();
 
             while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = line
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+                string cityName = tokens[1];
+                
+                City city = citys[cityName];
+                
+                if (command == "Plunder")
+                {
+                    int population = int.Parse(tokens[2]);
+                    int gold = int.Parse(tokens[3]);
+
+                    city.Population -= population;
+                    city.Gold -= gold;
+                    
+                    Console.WriteLine($"{cityName} plundered! {gold} gold stolen, {population} citizens killed.");
+                    
+
+                        if (city.Population <= 0 || city.Gold <= 0)
+                        {
+                            citys.Remove(cityName);
+
+                            Console.WriteLine($"{cityName} has been wiped off the map!");
+                        }
+                }
+
+                else if (command == "Prosper")
+                {
+                    int gold = int.Parse(tokens[2]);
+
+                    if (gold < 0)
+                    {
+                        Console.WriteLine("Gold added cannot be a negative number!");
+                        continue;
+                    }
+
+                    city.Gold += gold;
+
+                    Console.WriteLine($"{gold} gold added to the city treasury. {cityName} now has {city.Gold} gold.");
+                }
+            }
+
+            Dictionary<string, City> sorted = citys
+                .OrderByDescending(c => c.Value.Gold)
+                .ThenBy(c => c.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            if (sorted.Count == 0)
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
+            }
+
+            else
+            {
+                Console.WriteLine($"Ahoy, Captain! There are {citys.Count} wealthy settlements to go to:");
+
+                foreach (City city in sorted.Values)
+                {
+                    Console.WriteLine($" {city.Name} -> Population: {city.Population} citizens, Gold: {city.Gold} kg");
+                }
+            }
+        }
+        
+        private static Dictionary<string, City> ReadCity()
+        {
+         while (true)
             {
                 string line = Console.ReadLine();
 
@@ -52,81 +127,8 @@ namespace _03.P_rates
                     citys.Add(cityName, city);
                 }
             }
-
-            while (true)
-            {
-                string line = Console.ReadLine();
-
-                if (line == "End")
-                {
-                    break;
-                }
-
-                string[] tokens = line
-                    .Split("=>", StringSplitOptions.RemoveEmptyEntries);
-
-                string command = tokens[0];
-
-                if (command == "Plunder")
-                {
-                    string cityName = tokens[1];
-                    int population = int.Parse(tokens[2]);
-                    int gold = int.Parse(tokens[3]);
-
-                    Console.WriteLine($"{cityName} plundered! {gold} gold stolen, {population} citizens killed.");
-
-                    if (citys.ContainsKey(cityName))
-                    {
-                        City city = citys[cityName];
-                        city.Population -= population;
-                        city.Gold -= gold;
-
-                        if (city.Population <= 0 || city.Gold <= 0)
-                        {
-                            citys.Remove(cityName);
-
-                            Console.WriteLine($"{cityName} has been wiped off the map!");
-                        }
-                    }
-                }
-
-                else if (command == "Prosper")
-                {
-                    string townName = tokens[1];
-                    int gold = int.Parse(tokens[2]);
-
-                    if (gold < 0)
-                    {
-                        Console.WriteLine("Gold added cannot be a negative number!");
-                        continue;
-                    }
-
-                    City city = citys[townName];
-                    city.Gold += gold;
-
-                    Console.WriteLine($"{gold} gold added to the city treasury. {townName} now has {city.Gold} gold.");
-                }
-            }
-
-            Dictionary<string, City> sorted = citys
-                .OrderByDescending(c => c.Value.Gold)
-                .ThenBy(c => c.Key)
-                .ToDictionary(x => x.Key, x => x.Value);
-
-            if (sorted.Count == 0)
-            {
-                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
-            }
-
-            else
-            {
-                Console.WriteLine($"Ahoy, Captain! There are {citys.Count} wealthy settlements to go to:");
-
-                foreach (City city in sorted.Values)
-                {
-                    Console.WriteLine($" {city.Name} -> Population: {city.Population} citizens, Gold: {city.Gold} kg");
-                }
-            }
+            
+            return citys;
         }
     }
 }
